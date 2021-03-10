0d86fc625d221f1b94cfe4a37e0674b1b333fdca
Grady Tucker <gradytucker@Gradys-MBP.modem>
2021-03-10 12:34:11 +1100

    first commit

---
+++ constants/articles.js
@@ -0,0 +1,29 @@
+export default [
+  {
+    title: '[ Daily Quote ]',
+    image: 'https://blog.hubspot.com/hs-fs/hubfs/Sales_Blog/1-min.jpg?width=598&name=1-min.jpg',
+    //cta: 'View article', 
+    horizontal: true
+  },
+  {
+    title: 'Exercise 1',
+    image: 'https://images.unsplash.com/photo-1519368358672-25b03afee3bf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2004&q=80',
+    //cta: 'View article'
+  },
+  {
+    title: 'Exercise 2',
+    image: 'https://images.unsplash.com/photo-1500522144261-ea64433bbe27?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80',
+    //cta: 'View article' 
+  },
+  {
+    title: 'Exercise 3',
+    image: 'https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1326&q=80',
+    //cta: 'View article' 
+  },
+  {
+    title: 'Exercise 4',
+    image: 'https://images.unsplash.com/photo-1482686115713-0fbcaced6e28?fit=crop&w=1947&q=80',
+    //cta: 'View article', 
+    horizontal: true
+  },
+];
