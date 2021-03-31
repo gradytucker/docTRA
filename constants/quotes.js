2c2beb78bbe1da90d60dff9362109b39ad775189
William <khightofthenorn@gmail.com>
2021-03-31 13:07:23 +1100

    removed messy quotes, created constant file.

---
+++ constants/quotes.js
@@ -0,0 +1,13 @@
+const quotes_arr = [
+"Just like me, this person wishes to be happy and not to suffer ~ Jinpa 2016",
+"This person is someone's mother, father, son, daughter",
+"Anyone can get sick at any time, this person could be me or someone I care about",
+"You today. Me tomorrow",
+"All unskilful behaviour is the tragic expression of an unmet need ~ Marshall Rosenberg",
+"If you want others to be happy, practice compassion. If you want to be happy, practice compassion ~ Dalai Lama",
+"Compassion is the basis of all morality ~ Arthur Schopenhauer",
+"Simplicity, patience, compassion. These three are your greatest treasures ~ Lao Tzu",
+"The purpose of human life is to serve, and to show compassion and the will to help others ~ Albert Schweitzer"
+];
+
+export default quotes_arr;
