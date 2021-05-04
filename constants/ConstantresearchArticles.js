961b221dc34afde2f1dd1cd387f6223faca77fbe
gradytucker <gradytucker@icloud.com>
2021-05-04 13:05:33 +1000

    made research article pdf's embedded

--- constants/ConstantresearchArticles.js
+++ constants/ConstantresearchArticles.js
@@ -5,12 +5,15 @@ export default [
     title: 'Functional Neural Plasticity and Associated Changes in Postiive Affect After Compassion Training \n\n- Klimecki et al 2013',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlpmdgnsNADXwEINhMN8GwQEGeraGtFcd6bHln37SypTUX8yI26wiMdI_yUjzR058go-Q&usqp=CAU',
     //cta: 'View article'
-    URL: 'https://docs.google.com/viewerng/viewer?url=https://greatergood.berkeley.edu/images/uploads/Klimecki-PositiveAffectCompassionTraining.pdf',
+    URL: 'https://drive.google.com/viewerng/viewer?embedded=true&url=https://greatergood.berkeley.edu/images/uploads/Klimecki-PositiveAffectCompassionTraining.pdf',
   },
   {
     title: 'Differential Pattern of Functional Brain Plasticity After Compassion and Empathy Training \n\n-Klimecki et al 2014',
     image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlpmdgnsNADXwEINhMN8GwQEGeraGtFcd6bHln37SypTUX8yI26wiMdI_yUjzR058go-Q&usqp=CAU',
     //cta: 'View article' 
-    URL: 'https://docs.google.com/viewerng/viewer?url=https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4040103/pdf/nst060.pdf',
+    URL: 'https://drive.google.com/viewerng/viewer?embedded=true&url=https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4040103/pdf/nst060.pdf',
   },
 ];
+
+
+
