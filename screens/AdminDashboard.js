d215b7e5ef3e830f54995f82c387ed4b078daccf
jingyue <jdu39@student.monash.edu>
2021-05-08 15:39:55 +1000

    fetch all the userinformation on Admin Screen

--- screens/AdminDashboard.js
+++ screens/AdminDashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import {
+  View,
   ScrollView,
   StyleSheet,
   Image,
@@ -12,21 +13,57 @@ import { Block, Text, theme } from "galio-framework";
 import { Images, argonTheme } from "../constants";
 import FAQresponses from '../constants/FAQresponses';
 import { Card } from "../components";
+import { FlatList } from "react-native-gesture-handler";
+import firebase from "firebase";
 
 const { width } = Dimensions.get("screen");
 
 const thumbMeasure = (width - 48 - 32) / 3;
 const cardWidth = width - theme.SIZES.BASE * 2;
-
+var userList = []
 class Articles extends React.Component {
 
+  state = {
+    userList : null
+  }
+  fetchAllUserInformation = () =>{
+    firebase.database().ref('user-information').on('value', async snapshot =>{
+      let dataList = snapshot.val()
+      for(let i in dataList){
+        userList.push([i,dataList[i].name])
+      }
+      this.setState({userList:userList})
+    })
+  }
+
+  componentDidMount(){
+    this.fetchAllUserInformation()
+  }
+
+  componentWillUnmount(){
+    firebase.database().ref('user-information').off()
+  }
+
+  renderUserUsage = (item) =>{
+    return(
+      <View style = {styles.articles}>
+        <Block flex>
+        <Text bold size={20} color="#32325D">
+            {'\nUser_ID:'}
+          </Text>
+          <Block flex>
+            <Text bold size={16} color="#32325D"> 
+              {item[1]} 
+            </Text>
+          </Block>
+      </Block>
+    </View>
+    )
+  }
 
   renderCards = () => {
     return (
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        contentContainerStyle={styles.articles}>
-
+      <View style={styles.articles}>
         <Block flex>
           <Text bold size={28} color="#32325D">
             {'\nAdmin Dashboard:'}
@@ -35,23 +72,23 @@ class Articles extends React.Component {
             <Text bold size={22} color="#32325D">
               {'\n User data goes here:'}
             </Text>
-
+          </Block>
+          <Block flex row>
+            <FlatList data={this.state.userList}
+              renderItem={({ item }) => this.renderUserUsage(item)}
+              keyExtractor={(item, index) => index.toString()}>
+            </FlatList>
           </Block>
         </Block>
-      </ScrollView>
+        </View>
     );
   };
-
-
 
   render() {
     return (
       <Block flex center>
-        <ScrollView
-          showsVerticalScrollIndicator={false}
-        >
-          {this.renderCards()}
-        </ScrollView>
+        <FlatList  ListHeaderComponent={this.renderCards()}>
+        </FlatList>
       </Block>
     );
   }
